3559d7c7b83cef403c2390187329eade2d2b0c13
aidana03062665-png <aidana03062665@gmail.com>
2026-09-10 21:37:33 +0500

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BIOOLYMP_CONFIG = {
-  supabaseUrl: "ТВОЙ URL ИЗ SUPABASE",
-  supabaseAnonKey: "ТВОЙ sb_publishable_ КЛЮЧ",
+  supabaseUrl: "https://ynlaaoolujcdvxdvcsri.supabase.co",
+  supabaseAnonKey: "sb_publishable_k7v9Fz0fZ-tsZgxZBXwJAw_Ll1i1C_0",
   schoolName: "№1 Хромтау орта мектебі",
   teacherName: "Сисекенова Айдана",
   loginDomain: "bioolymp7.local"
